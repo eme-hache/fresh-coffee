d81e845800707677554e7935f944819437643be6
Eme Hache <luisalfonsodsg@outlook.com>
2023-12-15 21:56:53 -0500

    chore: searching production error

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,17 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-export default async function handler (req, res) {
-  try {
-    const prisma = new PrismaClient()
+export default async function handler(req, res) {
+  const prisma = new PrismaClient()
 
-    const categories = await prisma.category.findMany({
-      include: {
-        products: true,
-      }
-    })
+  const categories = await prisma.category.findMany({
+    include: {
+      products: true,
+    }
+  })
 
-    res.status(200).json(categories)
-  } catch (error) {
-    res.status(500).json({ error })
-  }
+  res.status(200).json(categories)
 }
